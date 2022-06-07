b0f01680051bda2ccf9300f7c00ab33c62693737
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-08 04:42:35 +0500

    Initial Commit

--- .space.kts
+++ .space.kts
@@ -4,7 +4,7 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Code analysis, up and and test") {
+job("Code analysis, plan, up and test") {
     container(displayName = "Sonarqube continuous inspection of code quality and security", image = "openjdk:11")
     {
         env["SONAR_TOKEN"] = Secrets("terraspace_infra_sonar_token")
