f5fbcc6886a8907dd3611b373968c955c657f62c
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-14 19:04:03 +0500

    Initial Commit

--- .space.kts
+++ .space.kts
@@ -55,7 +55,7 @@ job("Code analysis, plan, up and test") {
 
     container(displayName = "Sonarqube continuous inspection of code quality and security", image = "openjdk:11")
     {
-        env["SONAR_TOKEN"] = Secrets("terraspace_infra_sonar_token")
+        env["SONAR_TOKEN"] = Secrets("terraspace-infra_sonar_token")
         kotlinScript { api->
             api.gradlew("sonarqube")
         }
